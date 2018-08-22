29d679dc4b72b775f0bfd0e88e2ce15d45824552
Joed Machado <joedmg58@gmail.com>
2018-08-21 21:05:30 -0400

    Database schema and table model created

---
+++ models/contacts.js
@@ -0,0 +1,38 @@
+module.exports = function(sequelize, DataTypes) {
+  var Contacts = sequelize.define("Contacts", {
+    clientName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: { 
+        is: ["^[a-z]+$",'i'],
+        len: [1,20]
+      }
+    },
+    clientLastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate:  { 
+        is: ["^[a-z]+$",'i'],
+        len: [1,30]
+      }
+    },  
+    clientPhoneNumber: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isNumeric: true,
+        len: [10]
+      }
+    },
+    clientEmail: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: { isEmail: true }
+    },
+    clientDueDate: {
+      type: DataTypes.DATE,
+      validate: { isDate: true }
+    }
+  });
+  return Contacts;
+};
