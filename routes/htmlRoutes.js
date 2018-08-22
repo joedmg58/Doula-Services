c9b536392c799f0bffff532662495f3ad6a942cc
Joed Machado <joedmg58@gmail.com>
2018-08-21 21:29:16 -0400

    Working on html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,17 +3,17 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+    db.Contacts.findAll({}).then(function( data ) {
       res.render("index", {
         msg: "Welcome!",
-        examples: dbExamples
+        examples: data
       });
     });
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.get("/contacts/:id", function(req, res) {
+    db.Contacts.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
       res.render("example", {
         example: dbExample
       });
